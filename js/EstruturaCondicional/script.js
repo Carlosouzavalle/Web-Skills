0f773bf30e60ca22070e0b5153c3159e052ad570
Carlos Eduardo <carlosouzavalle@gmail.com>
2022-01-25 13:38:25 -0300

    objetos e estrutura condicional
    Objetos e estrutura condicional
    
    Iffs,else, else if, switch e etc...

---
+++ js/EstruturaCondicional/script.js
@@ -0,0 +1,49 @@
+var jogador1 = 0
+var jogador2 = 2
+var placar;
+
+// jogador1 != -1 && jogador2 != -1 ? console.log('Os jogadores são validos') : console.log('jogadoes invalidos')
+
+// if( jogador1 > 0)
+// {
+//     console.log('Jogador1 marcou ponto')
+// }
+// else {
+//     console.log('a partida esta 0 a 0')
+// }
+
+// else if(placar == 0) {
+//     console.log('vamos para os acressimos')
+// }
+
+
+
+let DayWeekend = 3;
+
+switch (DayWeekend) {
+    case 1:
+        console.log('Segunda')
+        break;
+    case 2:
+        console.log('Terça')
+        break;
+    case 3:
+        console.log('Quarta')
+        break;
+    case 4:
+        console.log('Quinta')
+        break;
+    case 5:
+        console.log('Sexta')
+        break;
+    case 6:
+        console.log('Sabado')
+        break;
+    case 7:
+        console.log('Domingo')
+        break;
+    default:
+        console.log('Deu Ruim')
+        break;
+}
+
---
+++ js/repetição/script.js
@@ -0,0 +1,39 @@
+// Laço for
+// Executa uma instrução até que ela seja falsa
+// for(let i = 0; i < 11; i++) 
+// {
+//     console.log(i)
+// }
+
+let array = ['0','1', '2', '3', '4', '5', '6', '7', 'aaa']
+
+//for in
+// for(let i in array)
+// {
+//     console.log(i)
+// }
+
+//for of
+//Executa a repetição a partir de valor
+// for(i of array)
+// {
+//     console.log(i)
+// }
+
+//while
+//Executa uma instrução enquanto a informação for verdadeira, a verificação é feita antes da execução
+// let a = 0
+// while(a < 10)
+// { 
+//     a++;
+//     console.log(a)
+// }
+
+
+// do while
+//Executa uma instrução até que determinada informação seja falsa, a verficação é feita depois da execução
+let a = 0
+do{
+    a++
+    console.log(a)
+} while(a < 10)
