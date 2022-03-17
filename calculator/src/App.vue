a3a3194f6879fcb58f12f520caa31ec248188393
Carlos Eduardo <carlosouzavalle@gmail.com>
2022-03-17 19:34:56 -0300

    Projeto calculadora em Vue

---
+++ calculator/src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="app">
+    <h1>Calculator</h1>
+    <Calculator />
+  </div>
+</template>
+
+<script>
+import Calculator from '../src/main/Calculator.vue'
+
+
+export default {
+  components: { Calculator}
+}
+</script>
+
+<style>
+
+  @font-face {
+    font-family: 'RobotoMono';
+    src: url("./fonts/RobotoMono-Thin.ttf");
+  }
+
+  * {
+      font-family: "RobotoMono", monospace;
+  }
+
+  body {
+    margin: 0;
+  }
+
+  #app{
+    display: flex;
+    flex-direction: column;
+    height: 100vh;
+    /* width: 1928px; */
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    color: #fff;
+    background: linear-gradient(to right, rgb(83, 105, 118), rgb(41, 46, 73));
+  }
+
+</style>
